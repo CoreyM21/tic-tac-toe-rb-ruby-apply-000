5cfa0611a097fe3a9bff2ce3b3ecbc918ade8264
Corey <coreyjmartin2@gmail.com>
2020-06-12 17:32:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,7 @@ end
 def turn_count(board)
    turns = 0
    board.each do |space|
-      if (spaces == "X") || (spaces == "O")
+      if (space == "X") || (space == "O")
          turns += 1
       end
    end
